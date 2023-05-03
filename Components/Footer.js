b040f2dbf72aab717341949d678a47a8bcfd486d
lioncoder8 <lioncoder8@gmail.com>
2023-05-03 15:20:56 +0900

    update footer and firebase

--- Components/Footer.js
+++ Components/Footer.js
@@ -9,7 +9,7 @@ function Footer() {
       <div className="bg-indigo-600 dark:bg-indigo-900">
         <div className="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
           <p className="text-gray-50 text-sm text-center sm:text-left">
-            © {year} Bits-Of-C0de —
+            © {year} PersonalBlogs —
             <a
               href="https://twitter.com/lioncoder8"
               rel="noopener noreferrer"
